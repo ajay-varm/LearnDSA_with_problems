7c1b5046c5a17a0232168a8c0e7faa7fdbc750b5
Ajay varma Bhupathiraju <85858862+ajay-varm@users.noreply.github.com>
2022-02-03 09:56:05 +0530

    added patterns in lecture-1

---
+++ src/Lecture-1/Patterns.java
@@ -0,0 +1,12 @@
+
+
+public class Patterns {
+    public static void main(String[] args)
+    {
+        System.out.print("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
