d9270c2c18d879a9a54abc3b9e523e6f91c1478b
Ajay varma Bhupathiraju <85858862+ajay-varm@users.noreply.github.com>
2022-02-05 23:38:50 +0530

    Lecture1 final commit

--- src/Lecture1/Problems/HelloWorld.java
+++ src/Lecture1/Problems/HelloWorld.java
@@ -4,12 +4,16 @@
 
 //Solution:
 
+// dont worry about the below line
 package Lecture1.Problems;
+
+// basic class initialization in java
 
 public class HelloWorld 
 {
     public static void main(String[] args)
     {
+        //This syntax was used for printing in java
         System.out.print("Hello, World!");
     }
 }
--- src/Lecture1/Problems/Patterns.java
+++ src/Lecture1/Problems/Patterns.java
@@ -9,6 +9,8 @@ public class Patterns
 {
         public static void main(String[] args)
     {
+// tip: println means it will print in line and when it comes to print it will continue n the same line.
+
         System.out.println("*");
         System.out.println("**");
         System.out.println("***");
